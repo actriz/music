9ea2175b60bee7ad653079f176b5cad4b7a4bb1f
raulgalis <rgalisteo2000@gmail.com>
2022-06-28 11:33:13 +0200

    done

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import os
 import sys
 l = input('Link:')
 if sys.platform == 'win32':
-    os.system(f"yt-dlp -x --audio-format mp3 {l} -o C:/Users/%username%/Downloads/%(title)s.%(ext)s")
+    os.system(f"yt-dlp -x --audio-format mp3 {l} -o C:/Users/%username%/Music/%(title)s.%(ext)s")
 else:
-    os.system(f"yt-dlp -f 'ba' -x --audio-format mp3 '{l}'  -o '~/Downloads/%(title)s.%(ext)s'")
+    os.system(f"yt-dlp -f 'ba' -x --audio-format mp3 '{l}'  -o '~/Music/%(title)s.%(ext)s'")
