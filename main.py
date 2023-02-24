dca8655ed59b98953ec0abd603dca8564a99d594
raulgalis <raulgalis@proton.me>
2023-02-24 22:31:58 +0100

    update

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
 import os
-import sys
-l = input('Link:')
-if sys.platform == 'win32':
-    os.system(f"yt-dlp -x --audio-format mp3 {l} -o C:/Users/%username%/Music/%(title)s.%(ext)s")
-else:
-    os.system(f"yt-dlp -f 'ba' -x --audio-format mp3 '{l}'  -o '~/Music/%(title)s.%(ext)s'")
+url = input('Link:')
+os.system(f"yt-dlp -f 'ba' -x --audio-format mp3 {url}  -o '~/Music/%(title)s.%(ext)s'")
