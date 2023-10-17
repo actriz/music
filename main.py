2c90a07f3c3704db2358276c8f91fca53d0801db
raulgalis <raulgalis@proton.me>
2023-10-17 21:09:17 +0200

    winlin

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import os
-url = input("URL:")
-path = f"C:/Users/{os.getenv("USERNAME")}/Music/%(title)s.%(ext)s"
-os.system(f"yt-dlp -f bestaudio -x --audio-format mp3 {url}  -o {path}")
+if os.name = 'nt'
+    url = input("URL:")
+    path = f"C:/Users/{os.getenv("USERNAME")}/Music/%(title)s.%(ext)s"
+    os.system(f"yt-dlp -f bestaudio -x --audio-format mp3 {url}  -o {path}")
+else:
+    url = input("URL:")
+    os.system(f"yt-dlp -f bestaudio -x --audio-format mp3 {url}  -o '~/Music/%(title)s.%(ext)s'")
