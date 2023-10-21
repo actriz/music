a3e44d3134ccf824beac99ffd87627eb12276721
raulgalis <raulgalis@proton.me>
2023-10-21 15:29:55 +0200

    error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-if os.name = 'nt'
+if os.name == 'nt':
     url = input("URL:")
     path = f"C:/Users/{os.getenv("USERNAME")}/Music/%(title)s.%(ext)s"
     os.system(f"yt-dlp -f bestaudio -x --audio-format mp3 {url}  -o {path}")
