5eec25e8446d6eaae5856c9396f99b7d1dcdde4f
actriz <raulgalis@proton.me>
2024-10-13 18:13:23 +0200

    update

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
+url = input("URL: ")
 if os.name == "nt":
-    url = input("URL:")
     path = f"C:/Users/{os.getenv("USERNAME")}/Music/%(title)s.%(ext)s"
     os.system(f"yt-dlp -f bestaudio -x --audio-format mp3 {url}  -o {path}")
 else:
-    url = input("URL:")
     os.system(f"yt-dlp -f bestaudio -x --audio-format mp3 {url} -o '~/Music/%(title)s.%(ext)s'")
